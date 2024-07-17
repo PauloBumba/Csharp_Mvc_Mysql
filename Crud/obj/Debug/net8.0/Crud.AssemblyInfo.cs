793311915eb0082aa5de6c024b67b1d702497b36
Paulo Mário Valente Bumba <143729787+PauloBumba@users.noreply.github.com>
2024-07-17 16:55:01 -0300

    Update

--- Crud/obj/Debug/net8.0/Crud.AssemblyInfo.cs
+++ Crud/obj/Debug/net8.0/Crud.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Crud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6baa60f1497787918d5278ee91fc1f1e3cd3f36c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+993ba21ef2368ed83eb2469ce16104d9c32ac9c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Crud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Crud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
